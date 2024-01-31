1ef0f93559e774af2510b9d18ff0e44dd8dd30ef
harichella005 <harichella005@gmail.com>
2024-01-31 20:16:06 +0530

    test file updated

--- test.py
+++ test.py
@@ -2,43 +2,13 @@
 import os
 import csv
 import requests
+from banner import main_banner, sub_banner, tld_banner
 
 """This is the main function use to call first and get the input from the user to select which feature should use, 
  if the user wants to use the Top level domain finder then he/she should enter the option 1 or the user wants to use
- the subdomain finder then he/she should enter the option 2 """
- 
- 
-def tld_banner():
-    print("  _______ _     _      ______ _           _                           ")
-    print(" |__   __| |   | |    |  ____(_)         | |                          ")
-    print("    | |  | | __| |    | |__   _ _ __   __| | ___ _ __                 ")
-    print("    | |  | |/ _` |    |  __| | | '_ \ / _` |/ _ \ '__|                ")
-    print("    | |  | | (_| |    | |    | | | | | (_| |  __/ |                   ")
-    print("    |_|  |_|\__,_|    |_|    |_|_| |_|\__,_|\___|_|                   ")
-                                                    
-                                                   
-def sub_banner():
-    print("   _____       _          ______ _           _                          ")
-    print("  / ____|     | |        |  ____(_)         | |                         ")
-    print(" | (___  _   _| |__      | |__   _ _ __   __| | ___ _ __                ")
-    print("  \___ \| | | | '_ \     |  __| | | '_ \ / _` |/ _ \ '__|               ")
-    print("  ____) | |_| | |_) |    | |    | | | | | (_| |  __/ |                  ")
-    print(" |_____/ \__,_|_.__/     |_|    |_|_| |_|\__,_|\___|_|                  ")
-                                                       
-                                                      
+ the subdomain finder then he/she should enter the option 2 """                                                                                                                                                 
 def main():
-    print("          _ _      ______ _           _                                  ")
-    print("    /\   | | |    |  ____(_)         | |                                 ")
-    print("   /  \  | | |    | |__   _ _ __   __| | ___ _ __                        ")
-    print("  / /\ \ | | |    |  __| | | '_ \ / _` |/ _ \ '__|                       ")
-    print(" / ____ \| | |    | |    | | | | | (_| |  __/ |                          ")
-    print("/_/    \_\_|_|    |_|    |_|_| |_|\__,_|\___|_|                          ")
-                                                
-                                                
-
-    print("\nOptions:")
-    print("1. TLD Finder (Top Level Domain )")
-    print("2. Subdomain Finder")
+    main_banner()
 
     option = input("\nEnter the number: ")
 
@@ -67,12 +37,13 @@ def tld():
                 r = requests.get(url, allow_redirects=False)
                 stscode = r.status_code
                 stat = f"{url} <------  [ {stscode} ]"
+                tlddomain = f"{domain}{tld}"
                 print(stat)
                 
                 with open(f"{domain}.txt", 'a+') as f:
-                    f.write(stat + '\n')
+                    f.write(tlddomain + '\n')
             except Exception:
-                print("URL NOT FOUND")
+                print("URL NOT FOUND\n")
     
 def subdomain():
     print("2 is chosen")
